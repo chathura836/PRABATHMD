ca36d132f67929349cd79ce5c06178c1229cff03
chathura836 <jayashankac31@gmail.com>
2025-01-03 13:02:13 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~qRszkILS#flXpxZALW8AhLG8oop5QXWvqNtrNQnb1l05VUXWxwu0"
 
 
 module.exports = {
